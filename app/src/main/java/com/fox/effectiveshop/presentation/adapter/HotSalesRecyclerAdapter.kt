4c30f21ac55fa6bfc335191ac071d8171162e391
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-11-05 22:03:35 +0300

    feat: add hot sales adapter

---
+++ app/src/main/java/com/fox/effectiveshop/presentation/adapter/HotSalesRecyclerAdapter.kt
@@ -0,0 +1,32 @@
+package com.fox.effectiveshop.presentation.adapter
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.fox.effectiveshop.databinding.HotSalesItemBinding
+
+class HotSalesRecyclerAdapter(private val imagesArray: IntArray): RecyclerView.Adapter<HotSalesRecyclerAdapter.HotSalesHolder>() {
+    private var hotSaleItems = arrayListOf<Int>()
+
+    class HotSalesHolder(var itemBinding: HotSalesItemBinding): RecyclerView.ViewHolder(itemBinding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HotSalesHolder {
+        val hotSalesItemBinding = HotSalesItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return HotSalesHolder(hotSalesItemBinding)
+    }
+
+    override fun onBindViewHolder(holder: HotSalesHolder, position: Int) {
+        holder.itemBinding.imagePlaceholder.setImageResource(imagesArray[position])
+    }
+
+    override fun getItemCount(): Int {
+        return imagesArray.size
+    }
+
+}
--- app/src/main/java/com/fox/effectiveshop/presentation/screens/MainScreenFragment.kt
+++ app/src/main/java/com/fox/effectiveshop/presentation/screens/MainScreenFragment.kt
@@ -7,9 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import com.fox.effectiveshop.R
 import com.fox.effectiveshop.databinding.FragmentMainScreenBinding
+import com.fox.effectiveshop.presentation.adapter.HotSalesRecyclerAdapter
 
 class MainScreenFragment : Fragment() {
     private lateinit var binding: FragmentMainScreenBinding
+    private var hotSalesRecyclerAdapter: HotSalesRecyclerAdapter? = null
+    private val images: IntArray = intArrayOf(
+        R.drawable.ellipse,
+        R.drawable.mock_best_seller,
+        R.drawable.test,
+        R.drawable.sakura
+    )
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -17,5 +26,14 @@ class MainScreenFragment : Fragment() {
         binding = FragmentMainScreenBinding.inflate(inflater)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        hotSalesRecyclerAdapter = HotSalesRecyclerAdapter(images)
+        binding.hotSalesViewPager.apply {
+            adapter = hotSalesRecyclerAdapter
+        }
+
     }
 }
